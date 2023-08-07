9363fb16efc350cab3cb328dfa3ea17b8026f5d9
Georgii174 <georgii_anisimov90@mail.ru>
2023-08-07 14:27:28 +0500

    test регистрации

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { Unauthorized } = require('../errors/unauthorized');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,5 +42,22 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function findOne(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Unauthorized('Неправильные почта или пароль'));
+          }
+          return user; // теперь user доступен
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
